6506bc2b16eea8bd0728f5a307d9d5ef95069a4a
pintermate225 <pintermate225@gmail.com>
2024-01-17 19:12:16 +0100

    Created skeleton for the project, added default database configuration in the application.properties.

---
+++ DSN/src/main/java/org/gfa/dsn/DsnApplication.java
@@ -0,0 +1,13 @@
+package org.gfa.dsn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DsnApplication.class, args);
+    }
+
+}
