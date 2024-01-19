8ca833e1802e3f177e52c08a58e2beb74df7eb6e
Patrik Vojta <vojta.patrik@gmail.com>
2024-01-19 17:05:33 +0100

    Add User entity and UserRepository

---
+++ DSN/src/main/java/org/gfa/dsn/model/User.java
@@ -0,0 +1,46 @@
+package org.gfa.dsn.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "full_name")
+    private String fullName;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "created_at", columnDefinition = "TIMESTAMP")
+    private LocalDateTime createdAt;
+
+    @Column(name = "last_modified_at", columnDefinition = "TIMESTAMP")
+    private LocalDateTime lastModifiedAt;
+}
+
---
+++ DSN/src/main/java/org/gfa/dsn/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package org.gfa.dsn.repository;
+
+import org.gfa.dsn.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+}
