a734a1fa35255a1e58ac821f82e4656dfef9fbb2
pintermate225 <133139840+pintermate225@users.noreply.github.com>
2024-01-19 14:43:59 +0100

    Application skeleleton  (#1)
    * Created skeleton for the project, added default database configuration in the application.properties.
    
    * deleted gradle.yml, because we use Maven

---
+++ DSN/src/test/java/org/gfa/dsn/DsnApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gfa.dsn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
