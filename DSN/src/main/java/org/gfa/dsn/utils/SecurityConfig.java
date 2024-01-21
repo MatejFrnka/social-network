4f1d519eb682a911a1031aa003138071185e0974
Patrik Vojta <vojta.patrik@gmail.com>
2024-01-21 19:58:18 +0100

    Add Security addition

--- DSN/src/main/java/org/gfa/dsn/utils/SecurityConfig.java
+++ DSN/src/main/java/org/gfa/dsn/utils/SecurityConfig.java
@@ -3,15 +3,20 @@ package org.gfa.dsn.utils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
     @Bean
     public static PasswordEncoder passwordEncoder() {
@@ -23,14 +28,38 @@ public class SecurityConfig {
         return configuration.getAuthenticationManager();
     }
 
+    /*
+    securityFilterChain info
+    - 1 Disable Cross-Site Request Forgery (CSRF)
+    - 2 The user should be authenticated for any request in the application.
+    - 3 Spring Security will never create an HttpSession and it will never use it to obtain the Security Context.
+    - 4 Spring Security’s HTTP Basic Authentication support is enabled by default. However, as soon as any
+    servlet-based configuration is provided, HTTP Basic must be explicitly provided.
+     */
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests(authorize -> authorize
-                        .anyRequest().authenticated()
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable()) // (1)
+                .authorizeRequests( auth -> auth
+                        .anyRequest().authenticated() // (2)
                 )
-                .formLogin(withDefaults())
-                .httpBasic(withDefaults());
-        return http.build();
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // (3)
+                .httpBasic(Customizer.withDefaults()) // (4)
+                .build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
+        UserDetails user = User.withUsername("user")
+                .password(passwordEncoder.encode("password"))
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withUsername("admin")
+                .password(passwordEncoder.encode("admin"))
+                .roles("USER", "ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
     }
 }
