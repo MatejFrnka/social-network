ea64e5ab25ff3035dca3c541ab9c9d2d064cbd09
Patrik Vojta <vojta.patrik@gmail.com>
2024-01-31 10:17:48 +0100

    Fix missing slashes

--- DSN/src/main/java/org/gfa/dsn/utils/SecurityConfig.java
+++ DSN/src/main/java/org/gfa/dsn/utils/SecurityConfig.java
@@ -47,12 +47,12 @@ public class SecurityConfig {
     }
 
     private static final String[] AUTH_WHITELIST = {
-            "swagger-ui.html",
-            "swagger-ui/**",
+            "/swagger-ui.html",
+            "/swagger-ui/**",
             "/swagger-resources/**",
             "/v3/api-docs/**",
             "/v3/api-docs.yaml",
-            "api/v1/auth/**",
+            "/api/v1/auth/**",
     };
 
     @Bean
