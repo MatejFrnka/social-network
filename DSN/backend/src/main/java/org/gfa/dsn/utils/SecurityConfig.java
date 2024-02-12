20c7a64b4bb2425783501bbfe09ac1440f51ca38
Patrik Vojta <vojta.patrik@gmail.com>
2024-02-12 19:38:21 +0100

    Reverting unwanted changes

--- DSN/backend/src/main/java/org/gfa/dsn/utils/SecurityConfig.java
+++ DSN/backend/src/main/java/org/gfa/dsn/utils/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig {
 
     private static final String[] AUTH_WHITELIST = {
             "/swagger-ui.html",
-            "/swagger-ui",
+            "/swagger-ui/**",
             "/swagger-resources/**",
             "/v3/api-docs/**",
             "/v3/api-docs.yaml",
