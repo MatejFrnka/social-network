e4f312a817bb50b0607725b399028d8c55b1e143
pintermate225 <pintermate225@gmail.com>
2024-02-14 16:27:31 +0100

    Added the layout to LandingPage.tsxn renamed layout.tsx to Layout.tsx

--- DSN/frontend/src/app/Layout.tsx
+++ DSN/frontend/src/app/Layout.tsx
@@ -11,12 +11,13 @@ type Props = {
 const Layout = ({ children, title = 'DeveloperHub' }: Props) => {
   return (
       <div className="layout">
-        <Head>
-          <title>{title}</title>
-          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" />
-        </Head>
-        <Header />
-        <main>{children}</main>
+          <Head>
+              <title>{title}</title>
+              <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"/>
+              <link rel="stylesheet" href="/styles/styles.css"/>
+          </Head>
+          <Header/>
+          <main>{children}</main>
         <Footer />
       </div>
   );
