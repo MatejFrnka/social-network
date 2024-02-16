e8130595a1a4d18e652ca6b1a3622218eea4080b
Patrik Vojta <vojta.patrik@gmail.com>
2024-02-16 18:54:46 +0100

    Swagger fixed

--- DSN/backend/src/main/java/org/gfa/dsn/controller/UserRestController.java
+++ DSN/backend/src/main/java/org/gfa/dsn/controller/UserRestController.java
@@ -1,7 +1,5 @@
 package org.gfa.dsn.controller;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import org.gfa.dsn.dto.*;
 import org.gfa.dsn.repository.UserRepository;
 import org.gfa.dsn.service.JwtService;
@@ -19,7 +17,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
-@Api(tags = "Endpoints")
 public class UserRestController {
     private final UserService userService;
     private final UserRepository userRepository;
@@ -35,19 +32,16 @@ public class UserRestController {
     }
 
     @GetMapping("/users")
-    @ApiOperation(value = "Get All Users")
     public List<UserDTO> getAllUsers() {
         return userService.getAllUsers();
     }
 
     @GetMapping("/users/{id}")
-    @ApiOperation(value = "Get Specific User By Id")
     public UserDTO getUser(@PathVariable Long id) {
         return userService.getUser(id);
     }
 
     @PostMapping("/signup")
-    @ApiOperation(value = "Register User")
     public ResponseEntity<?> registerUser(@RequestBody SignUpDTO signUpDTO) {
         if (userRepository.existsByUsername(signUpDTO.getUsername())) {
             ErrorDTO errorDTO = new ErrorDTO(HttpStatus.BAD_REQUEST.value(), "Username already exists!", System.currentTimeMillis());
@@ -62,7 +56,6 @@ public class UserRestController {
     }
 
     @PostMapping("/login")
-    @ApiOperation(value = "Login User")
     public JwtResponseDTO AuthenticateAndGetToken(@RequestBody LoginDTO loginDTO) {
         Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginDTO.getUsername(), loginDTO.getPassword()));
         if (authentication.isAuthenticated()) {
