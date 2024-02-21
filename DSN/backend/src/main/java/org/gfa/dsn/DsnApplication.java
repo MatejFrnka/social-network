6fe952ee125553cb9270e42adf4f4007a593c246
pintermate225 <pintermate225@gmail.com>
2024-02-21 17:46:44 +0100

    Added DsnApplication.java manually.

---
+++ DSN/backend/src/main/java/org/gfa/dsn/DsnApplication.java
@@ -0,0 +1,12 @@
+package org.gfa.dsn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DsnApplication.class, args);
+    }
+}
